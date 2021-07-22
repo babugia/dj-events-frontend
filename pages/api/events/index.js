1c307f820adc0a3f639e53d994e09e92cba7e88c
Babugia <guibbg12345@gmail.com>
2021-07-22 19:57:49 -0300

    create api, /events and /events/[slug] endpoints and use getStaticProps

---
+++ pages/api/events/index.js
@@ -0,0 +1,11 @@
+const { events } = require('./data.json');
+
+export default function handler(req, res) {
+  const { method } = req;
+  if (method === 'GET') {
+    res.status(200).json(events);
+  } else {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).json({ message: `Method ${method} is not allowed.` });
+  }
+}
