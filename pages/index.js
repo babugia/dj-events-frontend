165155260f2edb4e5bed9c370b21cce61562b56e
Babugia <guibbg12345@gmail.com>
2021-07-22 21:40:35 -0300

    feat: event-item and events pages

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
+import Link from 'next/link';
 import Layout from '@/components/layout';
+import EventItem from '@/components/event-item';
 import { API_URL } from '@/config/index';
 
 export default function Home({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((event) => (
+        <EventItem key={event.id} event={event} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href='/events'>
+          <a className='btn-secondary'>All Events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
@@ -15,7 +28,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      events,
+      events: events.slice(0, 3),
       revalidate: 1,
     },
   };
