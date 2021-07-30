21b50db6f36069b1fd3ab2f14569672ae9bd5e4f
Babugia <guibbg12345@gmail.com>
2021-07-29 21:40:41 -0300

    feat: create single event page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,75 @@
+import { FaPencilAlt, FaTimes } from 'react-icons/fa';
+import Link from 'next/link';
+import Image from 'next/image';
 import Layout from '@/components/layout';
+import { API_URL } from '@/config/index';
+import styles from '@/styles/event.module.css';
 
-export default function EventPage() {
+export default function EventPage({ event }) {
+  const deleteEvent = (e) => {
+    console.log('delete');
+  };
   return (
     <Layout>
-      <h1>My Event</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${event.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href='#' className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+        <span>
+          {event.date} at {event.time}
+        </span>
+        <h1>{event.name}</h1>
+        {event.image && (
+          <div className={styles.image}>
+            <Image
+              alt='Event banner'
+              src={event.image}
+              width={960}
+              height={600}
+            />
+          </div>
+        )}
+        <h3>Performers:</h3>
+        <p>{event.performers}</p>
+        <h3>Description:</h3>
+        <p>{event.description}</p>
+        <h3>Venue: {event.venue}</h3>
+        <p>{event.address}</p>
+
+        <Link href='/events'>
+          <a className={styles.back}> {'<'} Go Back</a>
+        </Link>
+      </div>
     </Layout>
   );
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map(({ slug }) => ({
+    params: { slug },
+  }));
+  return {
+    paths,
+    fallback: true, // https://nextjs.org/docs/basic-features/data-fetching#the-fallback-key-required
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+
+  return {
+    props: { event: events[0] },
+    revalidate: 1, // https://nextjs.org/docs/basic-features/data-fetching#incremental-static-regeneration
+  };
 }
